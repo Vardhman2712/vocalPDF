383d8d3d7dc6fc76ada7bdb39602c2697a982fef
Vardhman <113521400+Vardhman2712@users.noreply.github.com>
2025-01-22 02:50:18 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,19 @@
-import os
+from flask import request
 from app import app
+import os
+import pyttsx3
+
+@app.route('/speak', methods=['POST'])
+def speak():
+    text_to_speak = request.json.get('text')
+    
+    if text_to_speak:
+        engine = pyttsx3.init()
+        engine.say(text_to_speak)
+        engine.runAndWait()
+        return {"message": "Speech synthesis started"}
+    else:
+        return {"message": "No text provided"}
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
